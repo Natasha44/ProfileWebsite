9def2c3d9b5420497a5bca94b406a9e0f0d836c3
Natasha44 <whiteyrules14@hotmail.com>
2013-09-27 17:13:39 -0400

    Third Commit

--- js/main.js
+++ js/main.js
@@ -1,3 +1,9 @@
+/**File Name: main.js
+	Author Name: Natasha Whitehead
+	Website Name: Natasha Whitehead's Portfolio Website
+	File Discription: This is the JavaScript of the file which allows the slider to work.*/
+
+
 jQuery(document).ready(function(pic) {
 
           pic('#banner-fade').bjqs({
